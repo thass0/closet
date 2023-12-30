af0daecef05c671bace21a98e7fa9c7fc0045866
Thassilo Schulze <d4kd@proton.me>
2023-12-30 16:50:23 +0100

    Parse simple expressions

--- src/Doc.hs
+++ src/Doc.hs
@@ -1,37 +1,39 @@
 module Doc
   ( Doc (..)
-  , DocFrontMatter 
-  , DocProg (..)
-  , DocStmt (..)
+  , FrontMatter
+  , Prog (..)
+  , Stmt (..)
+  , Expr (..)
   , pDocument
-  , DocParser
+  , Parser
   ) where
 
 import qualified Data.Yaml as Y
 import qualified Data.ByteString.Char8 as BS
 import Control.Monad
 import Control.Applicative hiding (many, some)
+import Data.Functor ((<&>), ($>))
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
-import Text.Megaparsec.Char.Lexer as L
-import Data.Text hiding (empty, singleton)
+import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Text hiding (empty, singleton, null)
 import Data.Void (Void)
 import Data.Maybe (fromMaybe)
 
 data Doc = Doc
-  { docFrontMatter :: DocFrontMatter  -- ^ Front matter of the document.
-  , docProg :: [DocProg]  -- ^ Program to generate the document.
+  { docFrontMatter :: FrontMatter  -- ^ Front matter of the document.
+  , docProg :: [Prog]  -- ^ Program to generate the document.
   }
   deriving (Show, Eq)
 
-type DocFrontMatter = Y.Value
+type FrontMatter = Y.Value
 
-data DocProg
-  = DocStmt DocStmt
-  | DocLiteral Text
+data Prog
+  = Stmt Stmt
+  | LiteralContent Text
   deriving (Show, Eq)
 
-data DocStmt
+data Stmt
   = StmtIf
   | StmtFor
   | StmtAssign
@@ -40,22 +42,35 @@ data DocStmt
   | StmtCapture
   | StmtIncrement
   | StmtDecrement
-  | StmtExpress
+  | StmtExpress Expr
   deriving (Show, Eq)
 
-type DocParser = Parsec Void Text
+-- | TODO: Filters
+data Expr
+  = Var [Ident]
+  | StringLiteral Text
+  | Number Int  -- ^ TODO: There are floats and integers in liquid.
+  deriving (Show, Eq)
+
+type Ident = Text
+
+type Parser = Parsec Void Text
 
 -- | Skip one or more white space characters.
-spaceConsumer :: DocParser ()
+spaceConsumer :: Parser ()
 spaceConsumer = L.space space1 empty empty
 
 -- | Run the given parser and consume trailing white space.
-pLexeme :: DocParser a -> DocParser a
+pLexeme :: Parser a -> Parser a
 pLexeme = L.lexeme spaceConsumer
+
+-- | Consume any white space in the input.
+pSpace :: Parser ()
+pSpace = pLexeme space
 
 -- | Parse the given symbol. It must be followed by one or
 --   more white space characters.
-pSymbol :: Text -> DocParser Text
+pSymbol :: Text -> Parser Text
 pSymbol s = do
   void $ string s
   pLexeme space1
@@ -63,26 +78,85 @@ pSymbol s = do
 
 -- | Parse the given symbol. White space that follows the symbol
 --   is consumed.
-pSymbol' :: Text -> DocParser Text
+pSymbol' :: Text -> Parser Text
 pSymbol' = L.symbol spaceConsumer
 
+-- | Accept any character. Used together with @manyTill@.
+pAnything :: Parser Char
+pAnything = satisfy (const True)
+
+
+-- * Front Matter
+
 -- | Parse a document's front matter.
-pFrontMatter :: DocParser DocFrontMatter
+pFrontMatter :: Parser FrontMatter
 pFrontMatter = do
   void $ pSymbol "---"
-  dat <- someTill (satisfy (const True)) (try (eol *> string "---" *> eol))
+  dat <- someTill pAnything (try (eol *> string "---" *> eol))
   case Y.decodeEither' (BS.pack dat) of
     Left err -> error $ Y.prettyPrintParseException err
     Right obj -> pure obj
 
-pDocument :: DocParser Doc
+
+-- * Expressions
+
+pIdent :: Parser Ident
+pIdent = pIdentInner <&> pack
+  where
+    pIdentInner = (:) <$> (letterChar <|> char '_')  -- [A-Za-z_]
+                      <*> many (alphaNumChar <|> char '_')  -- [A-Za-z0-9_]*
+                      <?> "identifier"  -- Label
+
+pIdents :: Parser [Ident]
+pIdents =  sepBy1 pIdent (string ".")
+
+pInteger :: Parser Int
+pInteger = pLexeme L.decimal <?> "number"
+
+pString :: Parser Text
+pString = startChar *> bodyClosed <&> pack
+  where
+    startChar = char '\"' <?> "start of string"
+    bodyClosed = manyTill L.charLiteral endChar <?> "body of string"
+    endChar = char '\"' <?> "end of string"
+
+pExpr :: Parser Expr
+pExpr = choice $ try <$>
+  [ pIdents <&> Var
+  , pInteger <&> Number
+  , pString <&> StringLiteral
+  ]
+
+
+-- * Program content
+
+pStmt :: Parser Stmt
+pStmt = between (string "{{" >> pSpace) (pSpace >> string "}}") (pExpr <&> StmtExpress)
+
+stmtLookAhead :: Parser Text
+stmtLookAhead = lookAhead $ choice (string <$> ["{{", "{%", "{{-", "{%-"])
+
+pLiteralContent :: Parser Text
+pLiteralContent = someTill pAnything endOfContent <&> pack
+  where
+    endOfContent = (stmtLookAhead $> ()) <|> eof
+
+pProgram :: Parser Prog
+pProgram = choice
+  [ stmtLookAhead >> pStmt <&> Stmt
+  , pLiteralContent <&> LiteralContent
+  ]
+
+pDocument :: Parser Doc
 pDocument = do
   -- Consume initial space once. All other white space is
-  -- picked up by `pSymbol` and `pLexeme`.
+  -- picked up by @pSymbol@ and @pLexeme@.
   void Text.Megaparsec.Char.space
-  -- Parse an optional front matter.
+  -- Parse an optional front matter. If @---@ is found, we commit
+  -- to parsing the front matter and don't fall back.
   fm <- optional $ do
     void $ lookAhead (pSymbol "---")
     pFrontMatter
-  cont <- manyTill (satisfy (const True)) eof
-  pure $ Doc (fromMaybe (Y.object []) fm) [DocLiteral $ pack cont]
+  prog' <- many pProgram
+  let prog = if null prog' then [LiteralContent ""] else prog'
+  pure $ Doc (fromMaybe (Y.object []) fm) prog
--- test/DocSpec.hs
+++ test/DocSpec.hs
@@ -7,12 +7,34 @@ import Data.Text
 import Data.Either (isLeft)
 import qualified Data.Yaml as Y
 import Data.Yaml ((.=))
-import Doc
+import qualified Doc
+import qualified Doc
+
+
+-- * Helpers
+
+runTestParse :: Doc.Parser a -> Text -> Either String a
+runTestParse parser input =
+  case runParser parser "<in>" input of
+    Right x -> Right x
+    Left err -> Left (errorBundlePretty err)
+
+runDocParse :: Text -> Either String Doc.Doc
+runDocParse = runTestParse Doc.pDocument
+
+docWithEmptyFM :: [Doc.Prog] -> Doc.Doc
+docWithEmptyFM p = Doc.Doc
+        { docFrontMatter = Y.object []
+        , docProg = p
+        }
+
+-- * Specs
 
 spec :: Spec
 spec = do
     describe "Parse documents" $ do
         frontMatter
+        statement
 
 frontMatter :: Spec
 frontMatter = do
@@ -28,13 +50,13 @@ children:
     - Child3
 ---
 This is the content of this page!|]
-            (runTestParse pDocument input) `shouldBe` (Right $ Doc
+            runDocParse input `shouldBe` (Right $ Doc.Doc
                 { docFrontMatter = Y.object
                     [ "name" .= ("This is my name" :: Text)
                     , "age" .= (41 :: Int)
                     , "children" .= Y.array [ "Child1", "Child2", "Child3" ]
                     ]
-                , docProg = [ DocLiteral "This is the content of this page!" ]
+                , docProg = [ Doc.LiteralContent "This is the content of this page!" ]
                 })
 
         it "allow --- inside the front matter" $ do
@@ -43,34 +65,54 @@ This is the content of this page!|]
 title: "Blah --- my personal blog about Blah!"
 ---
 Blah is the best filler word possible.|] 
-            (runTestParse pDocument input) `shouldBe` (Right $ Doc
+            runDocParse input `shouldBe` (Right $ Doc.Doc
                 { docFrontMatter = Y.object
                     [ "title" .= ("Blah --- my personal blog about Blah!" :: Text) ]
-                , docProg = [ DocLiteral "Blah is the best filler word possible." ]
+                , docProg = [ Doc.LiteralContent "Blah is the best filler word possible." ]
                 })
         
         it  "closing --- must be at beginning of line" $ do
             let input1 = "---\r\ntitle: My blog ---\r\n" 
-            (runTestParse pDocument input1) `shouldSatisfy` isLeft
+            runDocParse input1 `shouldSatisfy` isLeft
             let input2Win = "---\r\ntitle: My blog\r\n---\r\n"
             let input2Unix = "---\ntitle: My blog\n---\n"
-            let input2Doc = Doc
+            let input2Doc = Doc.Doc
                     { docFrontMatter = Y.object [ "title" .= ("My blog" :: Text) ]
-                    , docProg = [ DocLiteral "" ]
+                    , docProg = [ Doc.LiteralContent "" ]
                     }
-            (runTestParse pDocument input2Win) `shouldBe` Right input2Doc
-            (runTestParse pDocument input2Unix) `shouldBe` Right input2Doc
+            runDocParse input2Win `shouldBe` Right input2Doc
+            runDocParse input2Unix `shouldBe` Right input2Doc
         
         it "commit to front matter after a single ---" $ do
             -- If the file contains a valid front matter start, the parser may not
             -- fall back to parsing this part of the file as normal file content.
             -- If the front matter that follows is invalid, the parser must fail entirely.
             let input = "---\ntitle: This blog has a front matter that does not end.\n"
-            (runTestParse pDocument input) `shouldSatisfy` isLeft
-            
+            runDocParse input `shouldSatisfy` isLeft
 
-runTestParse :: DocParser a -> Text -> Either String a
-runTestParse parser input =
-  case runParser parser "<file>" input of
-    Right x -> Right x
-    Left err -> Left (errorBundlePretty err)
+
+-- ** Statement tests
+
+statement :: Spec
+statement = do
+    describe "parse statements" $ do
+        expressStatement
+
+expressStatement :: Spec
+expressStatement = do
+    describe "Express statement" $ do
+        it "simple express statement" $ do
+            let parsedExpr e = Right (docWithEmptyFM [Doc.Stmt (Doc.StmtExpress e)])
+            runDocParse "{{ blah.x }}" `shouldBe`
+                    parsedExpr (Doc.Var ["blah", "x"])
+            runDocParse "{{ blah.x.y.hello.world }}" `shouldBe`
+                    parsedExpr (Doc.Var ["blah", "x", "y", "hello", "world"])
+            runDocParse "{{ \"Hello, world!\" }}" `shouldBe`
+                    parsedExpr (Doc.StringLiteral "Hello, world!")
+            runDocParse "{{ 543 }}" `shouldBe`
+                    parsedExpr (Doc.Number 543)
+        it "invalid express statement"$ do
+            runDocParse "{{ blah!x }}" `shouldSatisfy` isLeft
+            runDocParse "{{ blah.x. }}" `shouldSatisfy` isLeft
+            runDocParse "{{ blah * 91 }}" `shouldSatisfy` isLeft
+            runDocParse "{{ 514blah }}" `shouldSatisfy` isLeft
